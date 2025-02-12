8039ab4f91f212d42cd3de8e9cac001f5c03c0ed
Jorintas Butkus <joriskoris@protonmail.com>
2025-02-12 10:46:30 -0600

    settings update

--- dot_config/nvim/lua/configs/conform.lua
+++ dot_config/nvim/lua/configs/conform.lua
@@ -2,6 +2,8 @@ local options = {
   log_level = vim.log.levels.DEBUG,
   formatters_by_ft = {
     lua = { "stylua" },
+    typescript = { "prettierd", "prettier", stop_after_first = true },
+    typescriptreact = { "prettierd", "prettier", stop_after_first = true },
     python = { "ruff_organize_imports", lsp_format = "last" },
   },
 
